3b8d14cf36c18360a7cf261b4554a60c3c35f272
Yoann Diquélou <yoann.diquelou@gmail.com>
2022-06-22 20:33:12 +0200

    Amélioration du positionnement du texte

--- lib/src/main/java/fr/yoanndiquelou/boiteamartine/Drawer.java
+++ lib/src/main/java/fr/yoanndiquelou/boiteamartine/Drawer.java
@@ -4,12 +4,12 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.font.TextAttribute;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.imageio.ImageIO;
 
@@ -51,78 +50,98 @@ public class Drawer {
 
 	}
 
-
 	public BufferedImage processImage(Path source, String nom, String description)
 			throws IOException, FontFormatException {
 		long start = System.currentTimeMillis();
-		String format = source.toString().substring(source.toString().lastIndexOf(".") + 1);
-		Path target = Paths.get(source.getParent() + File.separator + UUID.randomUUID().toString() + format);
 
 		BufferedImage originalImage = ImageIO.read(source.toFile());
-		System.out.println("Read: " + (System.currentTimeMillis()-start));
-		// jpg needs BufferedImage.TYPE_INT_RGB
-		// png needs BufferedImage.TYPE_INT_ARGB
-		int type = -1;
-		if ("png".equalsIgnoreCase(format)) {
-			type = BufferedImage.TYPE_INT_ARGB;
-		} else if ("jpg".equalsIgnoreCase(format) || "jpeg".equalsIgnoreCase(format)) {
-			type = BufferedImage.TYPE_INT_RGB;
-		} else {
-			throw new IOException("Extension de fichier incorrect: " + format);
-		}
+
+		System.out.println("Read: " + (System.currentTimeMillis() - start));
 
 		// save an image
-//		ImageIO.write(process(originalImage, type, nom, description), format, target.toFile());
-		BufferedImage image =  process(originalImage, type, nom, description);
-		System.out.println("Process: " + (System.currentTimeMillis()-start));
+		BufferedImage image = process(originalImage, originalImage.getType(), nom, description);
+		System.out.println("Process: " + (System.currentTimeMillis() - start));
 		return image;
 	}
 
-	private float getFontSizeForWidth(String text, BufferedImage img, Font font, double width) {
-		float size = 10f;
-		FontMetrics metrics = img.getGraphics().getFontMetrics(font.deriveFont(size));
-		while (metrics.stringWidth(text) < width) {
-			size += 10;
-			metrics = img.getGraphics().getFontMetrics(font.deriveFont(size));
+	public void saveImage(BufferedImage image, Path target) throws IOException {
+		ImageIO.write(image, "PNG", target.toFile());
+
+	}
+
+	/**
+	 * Get font size according to font and write domain.
+	 * 
+	 * @param text     text to write
+	 * @param graphics graphics object
+	 * @param font     font to use
+	 * @param width    domain width
+	 * @param height   domain height
+	 * @return best size for text
+	 */
+	private float getFontSizeForSize(String text, Graphics graphics, Font font, double width, double height) {
+		float size = 1f;
+		FontMetrics metrics = graphics.getFontMetrics(font.deriveFont(size));
+		int stringHeight = metrics.getAscent() - metrics.getDescent();
+		while (metrics.stringWidth(text) < width && stringHeight < height) {
+			size += 1f;
+			metrics = graphics.getFontMetrics(font.deriveFont(size));
+			stringHeight = metrics.getAscent() - metrics.getDescent();
+			if (metrics.stringWidth(text) >= width) {
+				System.out.println("width");
+			}
+			if (stringHeight >= height) {
+				System.out.println("height");
+			}
 		}
-		return size - 10;
+		return size - 1f;
 	}
 
 	private BufferedImage process(BufferedImage old, int type, String nom, String description)
 			throws FontFormatException, IOException {
+		double widthOffsetRatio = 0.02;
+		double nameRatio = 0.5;
+		double descriptionRatio = 1 - nameRatio - 0.1;
+		int rectHeight = Double.valueOf(old.getHeight() * 0.30).intValue();
 
-		Color color = getHexColor(old);
+		float sizeLine1 = getFontSizeForSize(nom, old.getGraphics(), mFont, old.getWidth() * 0.9,
+				rectHeight * nameRatio);
+		float sizeLine2 = getFontSizeForSize(description, old.getGraphics(), mFont, old.getWidth() * 0.8,
+				rectHeight * descriptionRatio);
+		sizeLine2 = Math.min(sizeLine2, sizeLine1/2f);
 
-		old.getGraphics().fillRect(Double.valueOf(old.getWidth() * 0.05).intValue(), 0, old.getWidth(),
-				Double.valueOf(old.getHeight() / 3.4).intValue());
-		float sizeLine1 = getFontSizeForWidth(nom, old, mFont, old.getWidth() * 0.9);
+		Color color = getColor(old);
+		Graphics2D g2d = (Graphics2D) old.getGraphics();
+		g2d.fillRect(Double.valueOf(old.getWidth() * widthOffsetRatio).intValue(), 0, old.getWidth(), rectHeight);
 		AttributedString attributedText = new AttributedString(nom);
 		attributedText.addAttribute(TextAttribute.FONT, mFont.deriveFont(sizeLine1));
 		attributedText.addAttribute(TextAttribute.FOREGROUND, color);
 
 		FontMetrics metrics = old.getGraphics().getFontMetrics(mFont.deriveFont(sizeLine1));
 		int positionX = (old.getWidth() - metrics.stringWidth(nom)) / 2;
-//		int positionY = metrics.getAscent()-5;
-		int positionY = Double.valueOf(metrics.getHeight() / 2. + 50).intValue();
-		Graphics2D g2d = (Graphics2D) old.getGraphics();
+		int positionY = metrics.getAscent() - metrics.getDescent() + (int) (rectHeight * 0.20);
 		g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
 
 		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-//		old.getGraphics().drawString(attributedText.getIterator(), positionX, positionY);
 		g2d.drawString(attributedText.getIterator(), positionX, positionY);
-		float sizeLine2 = getFontSizeForWidth(description, old, mFont, old.getWidth() / 2);
 		attributedText = new AttributedString(description);
 		attributedText.addAttribute(TextAttribute.FOREGROUND, color);
 		attributedText.addAttribute(TextAttribute.FONT, mFont.deriveFont(sizeLine2));
 		FontMetrics metrics1 = old.getGraphics().getFontMetrics(mFont.deriveFont(sizeLine2));
-		int positionX1 = Double.valueOf(old.getWidth() - metrics1.stringWidth(description) - old.getWidth() / 8.)
+		int positionX1 = Double.valueOf(old.getWidth() - metrics1.stringWidth(description) - (old.getWidth() * 0.05))
 				.intValue();
-		int positionY1 = Double.valueOf(positionY + metrics1.getHeight() / 2. + metrics1.getAscent() / 2.).intValue();
+		int positionY1 = positionY + metrics1.getAscent() - metrics1.getDescent() + (int) (rectHeight * .1);
 		g2d.drawString(attributedText.getIterator(), positionX1, positionY1);
 		return old;
 	}
 
-	public static Color getHexColor(BufferedImage image) {
+	/**
+	 * Get main color of image.
+	 * 
+	 * @param image buffered image to process
+	 * @return main color
+	 */
+	public static Color getColor(BufferedImage image) {
 
 		Map<Integer, Integer> colorMap = new HashMap<>();
 		int height = image.getHeight();
@@ -142,10 +161,10 @@ public class Drawer {
 			}
 		}
 
-		return getMostCommonColor(colorMap);
+		return getMostCommonColor(colorMap, false);
 	}
 
-	private static Color getMostCommonColor(Map<Integer, Integer> map) {
+	private static Color getMostCommonColor(Map<Integer, Integer> map, boolean useAlpha) {
 		List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
 
 		Collections.sort(list, (Map.Entry<Integer, Integer> obj1,
@@ -153,20 +172,26 @@ public class Drawer {
 
 		Map.Entry<Integer, Integer> entry = list.get(list.size() - 1);
 		int[] rgb = getRGBArr(entry.getKey());
-
-		return new Color(rgb[0], rgb[1], rgb[2]);
-//        return "#" + Integer.toHexString(rgb[0])
-//                + Integer.toHexString(rgb[1])
-//                + Integer.toHexString(rgb[2]);
+		if (useAlpha) {
+			return new Color(rgb[0], rgb[1], rgb[2], rgb[3]);
+		} else {
+			return new Color(rgb[0], rgb[1], rgb[2], rgb[3]);
+		}
 	}
 
+	/**
+	 * Get Red Gren Blue Alphe from pixel value
+	 * 
+	 * @param pixel pixel value
+	 * @return table { red, green, blue, alpha }
+	 */
 	private static int[] getRGBArr(int pixel) {
 		int alpha = (pixel >> 24) & 0xff;
 		int red = (pixel >> 16) & 0xff;
 		int green = (pixel >> 8) & 0xff;
 		int blue = (pixel) & 0xff;
 
-		return new int[] { red, green, blue };
+		return new int[] { red, green, blue, alpha };
 	}
 
 	private static boolean isGray(int[] rgbArr) {
